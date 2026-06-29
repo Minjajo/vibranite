a152f9ced3cc590f54c2a8fa1e8767878b8ec5a3
Jaden <bigeyedan@gmail.com>
2026-06-28 18:40:46 -0600

    Initial commit

---
+++ src/main/java/net/minjajo/VibraniteDataGenerator.java
@@ -0,0 +1,11 @@
+package net.minjajo;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VibraniteDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
